3d4dc4cb6720a89f68ed1447f349dc2649a264e8
Francelino Marcilio <fmarcilio1989@gmail.com>
2020-03-28 19:34:29 -0300

    funcao de envio de email

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,9 +1,75 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native'
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import * as MailComposer from 'expo-mail-composer';
 
+import logoImg from '../../assets/logo.png';
+
+import style from './styles';
+import styles from '../Detail/styles';
 
 export default function Detail(){
+
+  const navigation = useNavigation();
+  const message = 'Olá APAD, estou entrando em contao para ajudar com o valor de R$ 200,00';
+
+  function navigateBack(){
+    navigation.goBack();
+  }
+
+  function sendMail(){
+    MailComposer.composeAsync({
+      subject: 'Herói do Caso: Cadelinha doente',
+      recipients: ['fran@gmail.com'],
+      body: message,
+    })
+  }
+
+  function sendWhatsapp(){
+
+  }
+
+  
   return(
-    <View />
+    <View style={style.container}>
+      <View style={style.header}>
+        <Image source={logoImg} />
+
+        <TouchableOpacity onPress={navigateBack}>
+          <Feather name="arrow-left" size={28} color="#E82041" />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.incident}>
+        <Text style={[styles.incidentPropety, {marginTop: 0}]}>ONG:</Text>
+        <Text style={styles.incidentValue}>APAD</Text>
+
+        <Text style={styles.incidentPropety}>CASO:</Text>
+        <Text style={styles.incidentValue}>Cadelinha doente</Text>
+
+        <Text style={styles.incidentPropety}>VALOR:</Text>
+        <Text style={styles.incidentValue}>R$ 120,00</Text>
+      </View>
+
+      <View style={styles.contactBox}>
+        <Text style={styles.heroTitle}>Salve o dia!</Text>
+        <Text style={styles.heroTitle}>Seja o Herói desse caso.</Text>
+
+        <Text style={style.heroDescription}>Entre em contato:</Text>
+
+        <View style={styles.actions}>
+          <TouchableOpacity style={styles.action} onPress={() => {}}>
+            <Text style={styles.actionText}>WhatsApp</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.actions}>
+          <TouchableOpacity style={styles.action} onPress={sendMail}>
+            <Text style={styles.actionText}>E-mail</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
   );
 }
--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 import { View, FlatList, Image, Text, TouchableOpacity } from 'react-native';
 
 import logoImg from '../../assets/logo.png';
@@ -7,6 +8,12 @@ import logoImg from '../../assets/logo.png';
 import styles from './styles';
 
 export default function Incidents(){
+  const navigation = useNavigation();
+
+  function navigateToDetail(){
+    navigation.navigate('Detail')
+  }
+
   return(
     <View style={styles.container}>
       <View style={styles.header}>
@@ -22,6 +29,8 @@ export default function Incidents(){
       <FlatList 
         data={[1, 2, 3]}
         style={styles.incidentList}
+        keyExtractor={incident => String(incident)}
+        showsVerticalScrollIndicator={false}
         renderItem={() => (
           <View style={styles.incident}>
           <Text style={styles.incidentPropety}>ONG:</Text>
@@ -35,7 +44,7 @@ export default function Incidents(){
 
           <TouchableOpacity 
             style={styles.detailsButton}
-            onPress={() => {}}
+            onPress={navigateToDetail}
           >
             <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
             <Feather name="arrow-right" size={16} color="#E02041" />
