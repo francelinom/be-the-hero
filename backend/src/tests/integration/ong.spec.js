21174dd98f6897c5b8baf08c51b742e9ec9690b3
Francelino Marcilio <fmarcilio1989@gmail.com>
2020-03-29 12:47:09 -0300

    criacao banco de testes e test de integration

---
+++ backend/src/tests/integration/ong.spec.js
@@ -0,0 +1,30 @@
+const request = require('supertest');
+const app = require('../../app');
+const connection = require('../../database/connection');
+
+describe('ONG', () =>{
+  beforeEach(async () =>{
+    await connection.migrate.rollback();
+    await connection.migrate.latest();
+  });
+
+  afterAll(async () => {
+    await connection.destroy();
+  });
+
+  it('should be able to create a new ONG', async () =>{
+    const response = await request(app)
+      .post('/ongs')
+      .send({
+        name: "APAD",
+        email: "contato@gmail.com",
+        whatsapp: "47000000000",
+        city: "Natal",
+        uf: "RN"
+      });
+
+      expect(response.body).toHaveProperty('id');
+      expect(response.body.id).toHaveLength(8);
+
+  });
+});
